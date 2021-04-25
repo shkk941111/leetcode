7a4e24fb3ca8de8c6311008af2917da30c82a17d
KaiChen <shkk941111@163.com>
2021-04-25 21:19:46 +0800

    Create MaximumDepthofBinaryTree1.cpp

---
+++ 104_Maximum Depth of Binary Tree/MaximumDepthofBinaryTree1.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+    /*    if(root == NULL) //若为根节点，深度为0
+            return 0;
+        return max(maxDepth(root->left),maxDepth(root->right)) + 1; //深度为左右之间大的那个加一
+        */
+    return root == NULL ? 0 : max(this->maxDepth(root->left),this->maxDepth(root->right)) + 1;
+    }
+};
