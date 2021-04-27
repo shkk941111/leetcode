f4a4d1c5cc123da3eaa9f706a020a9568e1725cc
KaiChen <shkk941111@163.com>
2021-04-27 17:01:36 +0800

    Update MergeTwoBinaryTrees.java

--- 0617_Merge Two Binary Trees/MergeTwoBinaryTrees.java
+++ 0617_Merge Two Binary Trees/MergeTwoBinaryTrees.java
@@ -15,16 +15,16 @@
  */
 
 class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null) {  //若第一个二叉树为空，则返回第二个二叉树作为合并后二叉树
-            return t2;
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {  //若第一个二叉树为空，则返回第二个二叉树作为合并后二叉树
+            return root2;
         }
-        if (t2 == null) {  //若第二个二叉树为空，则返回第一个二叉树作为合并后二叉树
-            return t1;
+        if (root2 == null) {  //若第二个二叉树为空，则返回第一个二叉树作为合并后二叉树
+            return root1;
         }
-        TreeNode merged = new TreeNode(t1.val + t2.val);  //创建新节点，值为两个二叉树之和
-        merged.left = mergeTrees(t1.left, t2.left);  //合并二叉树左支为两个二叉树左支之和
-        merged.right = mergeTrees(t1.right, t2.right);  //合并二叉树右支为两个二叉树右支之和
+        TreeNode merged = new TreeNode(root1.val + root2.val);  //创建新节点，值为两个二叉树之和
+        merged.left = mergeTrees(root1.left, root2.left);  //合并二叉树左支为两个二叉树左支之和
+        merged.right = mergeTrees(root1.right, root2.right);  //合并二叉树右支为两个二叉树右支之和
         return merged;  //返回合并后二叉树
     }
 }
