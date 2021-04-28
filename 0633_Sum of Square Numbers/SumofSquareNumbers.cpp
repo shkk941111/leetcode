d82afb2a0d76acbe6fd668038864deb789ec23f2
KaiChen <shkk941111@163.com>
2021-04-28 11:01:07 +0800

    Create SumofSquareNumbers.cpp

---
+++ 0633_Sum of Square Numbers/SumofSquareNumbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long b = sqrt(c);  //初始化b,对c开方，若要使a * a + b * b = c，则b <= sqrt(c)
+        for(long a = 0;a <= b;){
+        //while(a <= b){
+            long sum = a * a + b * b;  //求a与b的平方和
+            if(sum == c){  //若sum = c，输出true
+                return true;
+            }        
+            else if(sum < c){  //若sum < c，小值减一
+                a++;
+            }            
+            else{  //若sum > c，大值减一
+                b--;
+            }      
+        }
+        return false;  //若sum != c，输出false
+    }
+};
